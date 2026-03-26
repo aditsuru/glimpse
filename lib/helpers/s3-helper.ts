f901f15b52ea91224933540af8e31efb117d762e
Adi <me@aditsuru.com>
2026-03-26 22:33:07 +0530

    refactor: post.create endpoint to handle failure in more robust way

--- lib/helpers/s3-helper.ts
+++ lib/helpers/s3-helper.ts
@@ -2,12 +2,21 @@ import { CopyObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { StoragePrefix, s3 } from "@/lib/s3";
 
 // TODO: Update to use config module when credentials are available
-export async function confirmUpload(tempKey: string) {
-	const bucket = process.env.R2_BUCKET_NAME as string;
+export function getPermanentKeyAndUrl(tempKey: string) {
 	const permanentKey = tempKey.replace(
 		StoragePrefix.UPLOADS,
 		StoragePrefix.MEDIA
 	);
+
+	return {
+		fileUrl: `${process.env.R2_PUBLIC_URL}/${permanentKey}`,
+		permanentKey,
+	};
+}
+
+export async function confirmUpload(tempKey: string) {
+	const bucket = process.env.R2_BUCKET_NAME as string;
+	const { fileUrl, permanentKey } = getPermanentKeyAndUrl(tempKey);
 
 	await s3.send(
 		new CopyObjectCommand({
@@ -24,7 +33,7 @@ export async function confirmUpload(tempKey: string) {
 	);
 
 	return {
-		fileUrl: `${process.env.R2_PUBLIC_URL}/${permanentKey}`,
+		fileUrl,
 		fileKey: permanentKey,
 	};
 }
