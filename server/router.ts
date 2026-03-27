20326c33d617ed4d65091f9890e278d70fd70f20
Adi <me@aditsuru.com>
2026-03-27 20:03:45 +0530

    feat: final profile route

--- server/router.ts
+++ server/router.ts
@@ -2,6 +2,7 @@ import { bookmarkRouter } from "./modules/bookmark/bookmark.route";
 import { commentRouter } from "./modules/comment/comment.route";
 import { likeRouter } from "./modules/like/like.route";
 import { postRouter } from "./modules/post/post.route";
+import { profileRouter } from "./modules/profile/profile.route";
 import { uploadRouter } from "./modules/upload/upload.route";
 import { base } from "./os";
 
@@ -11,6 +12,7 @@ export const router = base.router({
 	comment: commentRouter,
 	like: likeRouter,
 	bookmark: bookmarkRouter,
+	profile: profileRouter,
 });
 
 export type Router = typeof router;
