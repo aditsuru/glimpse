84f95f157a86996aeaf69fc8e13b406d0a0be843
Adi <me@aditsuru.com>
2026-04-30 14:06:36 +0530

    feat: migrations, update codeGen script and profile.get endpoint

--- src/modules/profile/profile.route.ts
+++ src/modules/profile/profile.route.ts
@@ -1,3 +1,23 @@
-import { base } from "@/server/os";
+import { authedProcedure, base } from "@/server/os";
+import { profileSchema } from "./profile.schema";
+import { ProfileService } from "./profile.service";
 
-export const profileRouter = base.router({});
+const profileProcedure = authedProcedure.use(({ context, next }) => {
+	const profileService = new ProfileService(
+		context.db,
+		context.session.user.id
+	);
+	return next({
+		context: {
+			profileService,
+		},
+	});
+});
+
+export const profileRouter = base.router({
+	get: profileProcedure
+		.output(profileSchema.get.output)
+		.handler(async ({ context }) => {
+			return await context.profileService.get();
+		}),
+});
