7b0f76738ed6941a9159aef78fdaadcec5720781
Adi <me@aditsuru.com>
2026-05-02 19:10:43 +0530

    feat: follow endpoints; closes #98

--- src/modules/follow/follow.schema.ts
+++ src/modules/follow/follow.schema.ts
@@ -1,3 +1,48 @@
 import * as z from "zod";
 
-export const followSchema = {};
+export const followSchema = {
+	send: {
+		input: z.object({
+			targetUserId: z.string(),
+		}),
+		output: z.object({
+			success: z.boolean(),
+		}),
+	},
+
+	remove: {
+		input: z.object({
+			targetUserId: z.string(),
+		}),
+		output: z.object({
+			success: z.boolean(),
+		}),
+	},
+
+	accept: {
+		input: z.object({
+			followerId: z.string(),
+		}),
+		output: z.object({
+			success: z.boolean(),
+		}),
+	},
+
+	reject: {
+		input: z.object({
+			followerId: z.string(),
+		}),
+		output: z.object({
+			success: z.boolean(),
+		}),
+	},
+
+	removeFollower: {
+		input: z.object({
+			followerId: z.string(),
+		}),
+		output: z.object({
+			success: z.boolean(),
+		}),
+	},
+};
