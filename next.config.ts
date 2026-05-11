1f5a8ceb6aab0df1f2c3e62c05879433b1d60e25
Adi <me@aditsuru.com>
2026-05-11 16:53:14 +0530

    refactor: next config for s3 endpoint

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,39 @@
 import type { NextConfig } from "next";
 
 const isDev = process.env.NODE_ENV === "development";
-const hostname = process.env.NEXT_PUBLIC_APP_URL
+
+const appHostname = process.env.NEXT_PUBLIC_APP_URL
 	? new URL(process.env.NEXT_PUBLIC_APP_URL).hostname
 	: "localhost";
 
+const r2Hostname = process.env.NEXT_PUBLIC_R2_PUBLIC_URL
+	? new URL(process.env.NEXT_PUBLIC_R2_PUBLIC_URL).hostname
+	: "localhost";
+
 const nextConfig: NextConfig = {
-	allowedDevOrigins: [hostname],
+	allowedDevOrigins: [appHostname],
 
 	images: {
 		unoptimized: isDev,
 		remotePatterns: [
-			// Add dev origin for storage (MinIO bucket)
+			// Local — MinIO
 			...(isDev
 				? [
 						{
 							protocol: "http" as const,
-							hostname: hostname,
+							hostname: r2Hostname,
 							port: "9000",
 							pathname: "/**",
 						},
 					]
-				: []),
+				: [
+						// Staging/Prod — Cloudflare R2
+						{
+							protocol: "https" as const,
+							hostname: r2Hostname,
+							pathname: "/**",
+						},
+					]),
 		],
 	},
 };
